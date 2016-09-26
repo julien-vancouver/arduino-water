f1137bbab9f14ec0c3fec7266db735a236a167b5
julien-vancouver <judgejules04@gmail.com>
2016-09-25 22:19:45 -0700

    Add files via upload

--- sketch_watergarden/sketch_watergarden.ino
+++ sketch_watergarden/sketch_watergarden.ino
@@ -7,7 +7,6 @@
 */
 int moistureProbeInputPin = A0;
 int waterPumpPin = 2;
-int loopPumpPin = 3;
 
 /*
    define soil dry tolerances min & max
@@ -16,13 +15,13 @@ int loopPumpPin = 3;
           600   = Very Damp - stop watering
           <300  = Completely Saturated - call 911
 */
-int waterMoistureLevel = 823;
+int waterMoistureLevel = 800;
 
 /*
   delay the time between checking moisture value
   and to send the reading to the server
 */
-unsigned long pollMillis = 6000; //10 minutes(600000 (10 seconds for testing6000)
+unsigned long pollMillis = 3600000; //1 hour(3600000)
 
 /*
    When soil is dry enough to water
@@ -42,18 +41,28 @@ void setup() {
     Setup the pin modes for each pin declared
   */
   pinMode(moistureProbeInputPin, INPUT);
-  pinMode(waterPumpPin, OUTPUT);
-  pinMode(loopPumpPin, OUTPUT);
+  pinMode(waterPumpPin, OUTPUT); //solenoid pin for the water pump
+  digitalWrite(waterPumpPin, LOW); // make sure the pump is OFF
 
-/*
- * Send the variables to the timer.run funtion
- */
+  /*
+     Send the variables to the timer.run funtion
+  */
   timer.setInterval(pollMillis, poll);
 
   /*
      initialize serial communications at 9600 bps
   */
   Serial.begin(9600);
+
+  /*
+     Data logging to be included later
+     Use this section to setup the data logger
+     thinking of connecting a wifi shield and storing data on NAS for graphing
+     postToServer(water,moisture)
+
+     send the water status (whether the plants got water or not)
+     send the moisture level associated with water status
+  */
 }
 
 void loop() {
@@ -70,6 +79,7 @@ void poll() {
   int moisture = getSoilMoisture();
   int watered = 0;
   Serial.println(moisture);
+
   if (moisture > waterMoistureLevel) {
     waterThePlant();
     watered = 1;
@@ -77,12 +87,12 @@ void poll() {
   }
 
   /*
-     If the moisture level is OK
-     do not water and pause to check the moisture level again in 2 hours
+     If the moisture level is wet.
+     do not water and pause to check the moisture level again in 2 hours (7200000)
   */
   if (moisture < waterMoistureLevel) {
+    Serial.println(moisture);
     Serial.println("The soil is still wet, not watering yet");
-    delay(7200);
   }
 }
 
@@ -96,25 +106,24 @@ int getSoilMoisture() {
 }
 
 /*
-   Turn on the pump (led for testing) to
-   water the plant
+   Turn on the pump to water the plant
 */
 void waterThePlant() {
   Serial.println("Watering the plant for 3 seconds.");
   digitalWrite(waterPumpPin, HIGH); //turn the pump on
   delay(pumpOnMillis); //leave the pump on for pumpOnMillis(3 secs)
   digitalWrite(waterPumpPin, LOW); //turn the pump off
+
   Serial.println("Waiting 10 seconds for water to absorb, before measuring again.");
   delay(10000); //wait 10 seconds
+
+  /*
+      Test if soil has been watered enough
+      before leaving the watering cycle
+  */
   int moisture = getSoilMoisture();
   if (moisture > waterMoistureLevel) {
-    Serial.print("Soil is too dry, ");
-    Serial.print(moisture);
-    Serial.println(", keep watering");
-    digitalWrite(loopPumpPin, HIGH);
     waterThePlant();
-    digitalWrite(loopPumpPin, LOW);
-    Serial.println(" We have watered enough, turning pump off");
     delay(5000); //wait 5 seconds
   }
 }
